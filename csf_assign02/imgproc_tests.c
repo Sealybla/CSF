505a82ce4cdc91ccdb9eadcad99e19ab20c572fe
sealybla <lijialin2018@gmail.com>
2025-09-26 23:24:12 -0400

    changed tests

--- csf_assign02/imgproc_tests.c
+++ csf_assign02/imgproc_tests.c
@@ -406,22 +406,22 @@ void test_abs_diff(TestObjs *objs) {
   ASSERT( abs_diff(0, 255) == 255 ); 
 }
 
-void test_get_largest_abs_diff(TestObjs *objs) {
-  //nr, ng, nb, r, g, b
-  ASSERT( get_largest_abs_diff(10, 20, 30, 5, 15, 25) == 5 ); //nr
-  ASSERT( get_largest_abs_diff(10, 20, 30, 15, 5, 25) == 15 ); //ng
-  ASSERT( get_largest_abs_diff(10, 20, 30, 15, 25, 5) == 25 ); //nb
-  //tie all same
-  ASSERT( get_largest_abs_diff(10, 20, 30, 10, 20, 30) == 0 ); //all same
-  //tie nr/ng
-  ASSERT( get_largest_abs_diff(10, 20, 30, 5, 15, 30) == 5 ); //nr
-  //tie nr/nb
-  ASSERT( get_largest_abs_diff(10, 20, 30, 5, 20, 25) == 5 ); //nr
-  //tie ng/nb
-  ASSERT( get_largest_abs_diff(10, 20, 30, 10, 5, 25) == 15 ); //ng   
-  //negative diff
-  ASSERT( get_largest_abs_diff(5, 15, 25, 10, 20, 30) == -5 ); //nr
-  ASSERT( get_largest_abs_diff(15, 5, 25, 10, 20, 30) == -15 ); //ng
-  ASSERT( get_largest_abs_diff(25, 15, 5, 10, 20, 30)  == -25 ); //nb
+// void test_get_largest_abs_diff(TestObjs *objs) {
+//   //nr, ng, nb, r, g, b
+//   ASSERT( get_largest_abs_diff(10, 20, 30, 5, 15, 25) == 5 ); //nr
+//   ASSERT( get_largest_abs_diff(10, 20, 30, 15, 5, 25) == 15 ); //ng
+//   ASSERT( get_largest_abs_diff(10, 20, 30, 15, 25, 5) == 25 ); //nb
+//   //tie all same
+//   ASSERT( get_largest_abs_diff(10, 20, 30, 10, 20, 30) == 0 ); //all same
+//   //tie nr/ng
+//   ASSERT( get_largest_abs_diff(10, 20, 30, 5, 15, 30) == 5 ); //nr
+//   //tie nr/nb
+//   ASSERT( get_largest_abs_diff(10, 20, 30, 5, 20, 25) == 5 ); //nr
+//   //tie ng/nb
+//   ASSERT( get_largest_abs_diff(10, 20, 30, 10, 5, 25) == 15 ); //ng   
+//   //negative diff
+//   ASSERT( get_largest_abs_diff(5, 15, 25, 10, 20, 30) == -5 ); //nr
+//   ASSERT( get_largest_abs_diff(15, 5, 25, 10, 20, 30) == -15 ); //ng
+//   ASSERT( get_largest_abs_diff(25, 15, 5, 10, 20, 30)  == -25 ); //nb
   
-}
+// }
